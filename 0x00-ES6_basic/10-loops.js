6080be5d72bc2f926a43936f3c8b01ce77ab6885
princekelvx@gmail.com <[princekelvx@gmail.com]>
2023-01-23 14:17:12 -0800

    changed code structure

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
   for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+    const value = idx;
+    arr.push(appendString + value);
   }
 
-  return array;
+  return arr;
 }
