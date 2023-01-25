5159da73efe253c1b3ce744284e41a609996ffc1
princekelvx@gmail.com <[princekelvx@gmail.com]>
2023-01-25 09:12:06 -0800

    first commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import { resolve } from "path";
+
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (success === true) {
+      resolve(object);
+    } else {
+      reject(Error("The fake API is not working currently"));
+    }
+  });
+}
