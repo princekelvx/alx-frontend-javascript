48444cf7e55848253b1a9594f74694935e43ac31
princekelvx@gmail.com <[princekelvx@gmail.com]>
2023-01-25 10:09:06 -0800

    error fixing

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,5 +7,5 @@ export default function handleProfileSignup() {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
 
-    .catch(() => console.log("Signuo system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
